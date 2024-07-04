355f1a2492aa9972216809d57f95545e4806db18
Krzysztof Chudziak <krzysztof.chudziak2000@gmail.com>
2024-07-04 19:35:15 +0200

    updating tests

--- app/test_letter_count.py
+++ app/test_letter_count.py
@@ -4,8 +4,8 @@ Testing a dice throw
 """
 import pytest  # type: ignore
 import matplotlib.pyplot as plt # type: ignore
-from .tools import rzut_kostka
-from .tools import rzut_moneta
+from tools import rzut_kostka
+from tools import rzut_moneta
 
 
 
